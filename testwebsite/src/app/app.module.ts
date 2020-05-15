866710e3541eb94dbdf473d12d61befea9d2c4d8
Frank Du <yu.du.19@ucl.ac.uk>
2020-05-15 18:46:56 +0100

    test config

--- testwebsite/src/app/app.module.ts
+++ testwebsite/src/app/app.module.ts
@@ -2,10 +2,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
-// used to create fake backend
-import { fakeBackendProvider } from './_helpers';
-
 import { appRoutingModule } from './app.routing';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
@@ -31,9 +27,6 @@ import { AlertComponent } from './_components';
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-
-        // provider used to create fake backend
-        fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
